2e10a0f2e5c5fe2d1a44ce1d9c9596e9296dde64
WyattYuan <Wyatt049@outlook.com>
2026-02-17 23:04:08 +0800

    Project 1B: Guitar

--- proj1b/src/gh2/GuitarString.java
+++ proj1b/src/gh2/GuitarString.java
@@ -1,5 +1,6 @@
 package gh2;
 
+import deque.ArrayDeque61B;
 import deque.Deque61B;
 
 // TODO: maybe more imports
@@ -13,8 +14,7 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque61B<Double> buffer;
+    private Deque61B<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
@@ -22,6 +22,11 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer with zeros.
+        int capacity = Math.toIntExact(Math.round(SR / frequency));
+        buffer = new ArrayDeque61B<Double>();
+        for (int i = 0; i < capacity; i++) {
+            buffer.addLast(0.0);
+        }
     }
 
 
@@ -35,6 +40,11 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+        int capacity = buffer.size();
+        for (int i = 0; i < capacity; i++) {
+            buffer.removeFirst();
+            buffer.addLast(Math.random() - 0.5);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
@@ -44,12 +54,14 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       **Do not call StdAudio.play().**
+        double oldFront = buffer.removeFirst();
+        double newFront = buffer.get(0);
+        buffer.addLast(DECAY * 0.5 * (oldFront + newFront));
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        return buffer.get(0);
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
