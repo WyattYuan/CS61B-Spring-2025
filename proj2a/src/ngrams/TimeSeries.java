88a52ebcc7c5b26c73a94cdfcd2558c2ec130afd
WyattYuan <Wyatt049@outlook.com>
2026-03-12 16:46:23 +0800

    proj2a: 完成TimeSeries类

--- proj2a/src/ngrams/TimeSeries.java
+++ proj2a/src/ngrams/TimeSeries.java
@@ -1,10 +1,11 @@
 package ngrams;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
 /**
- * An object for mapping a year number (e.g. 1996) to numerical data. Provides
+ * An object for mapping a year number (e.g., 1996) to numerical data. Provides
  * utility methods useful for data analysis.
  *
  * @author Josh Hug
@@ -24,21 +25,24 @@ public class TimeSeries extends TreeMap<Integer, Double> {
         super();
     }
 
+    public TimeSeries(TimeSeries ts){
+        super(ts);
+    }
+
     /**
      * Creates a copy of TS, but only between STARTYEAR and ENDYEAR,
      * inclusive of both end points.
      */
     public TimeSeries(TimeSeries ts, int startYear, int endYear) {
-        super();
-        // TODO: Fill in this constructor.
+        super(ts.subMap(startYear,endYear+1)); // 左闭右开
     }
 
     /**
      *  Returns all years for this time series in ascending order.
      */
     public List<Integer> years() {
-        // TODO: Fill in this method.
-        return null;
+        // keySet 方法返回的就是正序的结果
+        return new ArrayList<>(super.keySet());
     }
 
     /**
@@ -46,38 +50,47 @@ public class TimeSeries extends TreeMap<Integer, Double> {
      *  order of years().
      */
     public List<Double> data() {
-        // TODO: Fill in this method.
-        return null;
+        return new ArrayList<>(super.values());
     }
 
     /**
      * Returns the year-wise sum of this TimeSeries with the given TS. In other words, for
      * each year, sum the data from this TimeSeries with the data from TS. Should return a
      * new TimeSeries (does not modify this TimeSeries).
-     *
+     * <p>
      * If both TimeSeries don't contain any years, return an empty TimeSeries.
      * If one TimeSeries contains a year that the other one doesn't, the returned TimeSeries
      * should store the value from the TimeSeries that contains that year.
      */
     public TimeSeries plus(TimeSeries ts) {
-        // TODO: Fill in this method.
-        return null;
+        TimeSeries result = new TimeSeries(ts);
+        this.forEach((key, value) ->
+                result.merge(key, value, Double::sum)
+        );
+        return result;
     }
 
     /**
      * Returns the quotient of the value for each year this TimeSeries divided by the
      * value for the same year in TS. Should return a new TimeSeries (does not modify this
      * TimeSeries).
-     *
+     * <p>
      * If TS is missing a year that exists in this TimeSeries, throw an
      * IllegalArgumentException.
      * If TS has a year that is not in this TimeSeries, ignore it.
      */
     public TimeSeries dividedBy(TimeSeries ts) {
-        // TODO: Fill in this method.
-        return null;
-    }
+        TimeSeries result = new TimeSeries(this);
 
-    // TODO: Add any private helper methods.
-    // TODO: Remove all TODO comments before submitting.
+        this.forEach((key, value) ->
+                {
+                    Double divisor = ts.get(key);
+                    if (divisor == null){
+                        throw new IllegalArgumentException("TS must contain all years in this TimeSeries");
+                    }
+                    result.replace(key, value/divisor);
+                }
+        );
+        return result;
+    }
 }
--- proj2a/tests/TimeSeriesTest.java
+++ proj2a/tests/TimeSeriesTest.java
@@ -2,6 +2,7 @@ import ngrams.TimeSeries;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -60,5 +61,78 @@ public class TimeSeriesTest {
 
         assertThat(totalPopulation.years()).isEmpty();
         assertThat(totalPopulation.data()).isEmpty();
+    }
+
+    @Test
+    public void testConstructor(){
+        TimeSeries catPopulation = new TimeSeries();
+        catPopulation.put(1991, 0.0);
+        catPopulation.put(1992, 100.0);
+        catPopulation.put(1994, 200.0);
+
+        TimeSeries x = new TimeSeries(catPopulation,1991,1992);
+        assertThat(x.years()).isEqualTo(Arrays.asList(1991,1992));
+    }
+
+    @Test
+    public void testYears(){
+        TimeSeries catPopulation = new TimeSeries();
+        catPopulation.put(1992, 100.0);
+        catPopulation.put(1991, 0.0);
+        catPopulation.put(1994, 200.0);
+        catPopulation.put(2000, 200.0);
+
+        List<Integer> expected = new ArrayList<>();
+        expected.addLast(1991);
+        expected.addLast(1992);
+        expected.addLast(1994);
+        expected.addLast(2000);
+        assertThat(catPopulation.years()).isEqualTo(expected);
+
+        System.out.println(catPopulation.values());
+
+        TimeSeries catPopulationCopy = new TimeSeries(catPopulation);
+        assertThat(catPopulationCopy.years()).isEqualTo(expected);
+    }
+
+    @Test
+    public void testDivision() {
+        TimeSeries catPopulation = new TimeSeries();
+        catPopulation.put(1991, 0.0);
+        catPopulation.put(1992, 100.0);
+        catPopulation.put(1994, 300.0);
+
+        TimeSeries dogPopulation = new TimeSeries();
+        dogPopulation.put(1991, 1.0);
+        dogPopulation.put(1992, 200.0);
+        dogPopulation.put(1994, 400.0);
+        dogPopulation.put(1995, 500.0);
+
+        TimeSeries fishPopulation = new TimeSeries();
+        fishPopulation.put(1991, 1.0);
+        fishPopulation.put(1992, 200.0);
+        fishPopulation.put(1995, 400.0);
+
+        TimeSeries totalPopulation = catPopulation.dividedBy(dogPopulation);
+        // expected: 1991: 0,
+        //           1992: 0.5
+        //           1994: 0.75
+
+
+        List<Double> expectedTotal = new ArrayList<>();
+        expectedTotal.add(0.0);
+        expectedTotal.add(0.5);
+        expectedTotal.add(0.75);
+
+        for (int i = 0; i < expectedTotal.size(); i += 1) {
+            assertThat(totalPopulation.data().get(i)).isWithin(1E-10).of(expectedTotal.get(i));
+        }
+
+        try {
+            totalPopulation.dividedBy(fishPopulation);
+        } catch(Exception e) {
+            // 测试是否抛出错误的类型
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 } 
