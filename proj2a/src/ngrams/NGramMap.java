ef9cc7540e0ad61fe168641ba775494d33001603
WyattYuan <Wyatt049@outlook.com>
2026-03-24 17:56:50 +0800

    完成NGramMap构造函数并解析输入文件

--- proj2a/src/ngrams/NGramMap.java
+++ proj2a/src/ngrams/NGramMap.java
@@ -1,14 +1,17 @@
 package ngrams;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
-
-import static ngrams.TimeSeries.MAX_YEAR;
-import static ngrams.TimeSeries.MIN_YEAR;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 /**
  * An object that provides utility methods for making queries on the
  * Google NGrams dataset (or a subset thereof).
- *
+ * <p>
  * An NGramMap stores pertinent data from a "words file" and a "counts
  * file". It is not a map in the strict sense, but it does provide additional
  * functionality.
@@ -18,12 +21,63 @@ import static ngrams.TimeSeries.MIN_YEAR;
 public class NGramMap {
 
     // TODO: Add any necessary static/instance variables.
+    HashMap<String, TimeSeries> wordTimeSeries;  // 单词及其TS对应
+    TimeSeries totalCounts;  // 所有单词的总计TS
 
     /**
      * Constructs an NGramMap from WORDSFILENAME and COUNTSFILENAME.
+     * WordsFile是每行第一项是单词。
+     * 第二项是年份。
+     * 第三项是该单词在该年出现在任何书籍中的次数。
+     * 第四项是包含该单词的不同来源的数量。你的程序应该忽略第四列。
+     * <p>
+     * COUNTSFILE
+     * 每行第一项是年份。
+     * 第二项是该年度所有文本记录的总字数。
+     * 第三项是该年度文本的总页数。
+     * 第四项是该年度不同来源的总数。你的程序应该忽略第三列和第四列。
      */
     public NGramMap(String wordsFilename, String countsFilename) {
-        // TODO: Fill in this constructor. See the "NGramMap Tips" section of the spec for help.
+        // 需要一个数据结构，一个word对应一个TimeSeries
+        // 感觉是 HashMap
+        wordTimeSeries = new HashMap<>();
+        totalCounts = new TimeSeries();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(wordsFilename))) {
+            String line;
+            String lastWord = null;
+            TimeSeries lastTimeSeries = new TimeSeries();
+            while ((line = br.readLine()) != null) {
+                // 指定分隔符为\t
+                StringTokenizer st = new StringTokenizer(line, "\t");
+                String currentWord = st.nextToken();
+                if (!currentWord.equals(lastWord) && lastWord != null) {
+                    // 新单词
+                    // 把上个单词放进去
+                    wordTimeSeries.put(lastWord, new TimeSeries(lastTimeSeries));
+                    lastTimeSeries.clear();
+                }
+                Integer year = Integer.parseInt(st.nextToken());
+                Double wordFreq = Double.parseDouble(st.nextToken());
+                lastTimeSeries.put(year, wordFreq);
+                lastWord = currentWord;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (BufferedReader br2 = new BufferedReader(new FileReader(countsFilename))) {
+            String line;
+            while ((line = br2.readLine()) != null) {
+                // 指定分隔符为\t
+                StringTokenizer st = new StringTokenizer(line, ",");
+                Integer year = Integer.parseInt(st.nextToken());
+                Double count = Double.parseDouble(st.nextToken());
+                totalCounts.put(year, count);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
